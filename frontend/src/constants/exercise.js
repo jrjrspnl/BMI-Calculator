dc1d67607e0f25267a5a3dfb3845f8a833f29828
jrjrspnl <jerwinespenile@gmail.com>
2025-10-22 13:32:56 +0800

    add exercises

---
+++ frontend/src/constants/exercise.js
@@ -0,0 +1,36 @@
+const exerciseData = {
+  exercises: [
+    {
+      name: "Walking",
+      tags: ["low-impact", "cardio", "Obesity"],
+      source: "https://via.placeholder.com/300x200?text=Healthy+Meal+Plan",
+      image: "https://via.placeholder.com/300x200?text=Healthy+Meal+Plan",
+    },
+    {
+      name: "Swimming",
+      tags: ["cardio", "Obesity", "full-body"],
+      source: "https://via.placeholder.com/300x200?text=Healthy+Meal+Plan",
+      image: "https://via.placeholder.com/300x200?text=Healthy+Meal+Plan",
+    },
+    {
+      name: "Push-ups",
+      tags: ["strength", "Overweight", "upper-body"],
+      source: "https://via.placeholder.com/300x200?text=Healthy+Meal+Plan",
+      image: "https://via.placeholder.com/300x200?text=Healthy+Meal+Plan",
+    },
+    {
+      name: "Jogging",
+      tags: ["cardio", "Healthy", "endurance"],
+      source: "https://via.placeholder.com/300x200?text=Healthy+Meal+Plan",
+      image: "https://via.placeholder.com/300x200?text=Healthy+Meal+Plan",
+    },
+    {
+      name: "Yoga",
+      tags: ["flexibility", "Underweight", "relaxation"],
+      source: "https://via.placeholder.com/300x200?text=Healthy+Meal+Plan",
+      image: "https://via.placeholder.com/300x200?text=Healthy+Meal+Plan",
+    },
+  ],
+};
+
+export default exerciseData;
